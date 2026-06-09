8474fb14e114f4c8985441752f79a27bf9ecd32b
Brijesh Balaji S <143893586+BrijeshBalaji@users.noreply.github.com>
2026-06-09 15:22:25 +0530

    feat: Add Count Number of Nice Subarrays

--- Array/Count Number of Nice Subarrays/java/Solution.java
+++ Array/Count Number of Nice Subarrays/java/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         map.put(0, 1);
         int sum = 0, count = 0;
         for (int num : nums) {
-            if(i%2!=0)
+            if(num%2!=0)
             sum += num%2;
             count += map.getOrDefault(sum - k, 0);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
