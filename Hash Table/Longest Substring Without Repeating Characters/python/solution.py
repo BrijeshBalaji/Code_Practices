bf7cb29965f608243482a3c22e9fc7c8d2aae948
Brijesh Balaji S <143893586+BrijeshBalaji@users.noreply.github.com>
2026-06-11 16:03:45 +0530

    feat: Add Longest Substring Without Repeating Characters

--- Hash Table/Longest Substring Without Repeating Characters/python/solution.py
+++ Hash Table/Longest Substring Without Repeating Characters/python/solution.py
@@ -1,5 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        
+        int n = s.length();
+        int maxLength = 0;
+        int left = 0;
+        int[] index = new int[128]; 
+        for (int right = 0; right < n; right++) {
+            char c = s.charAt(right);
+            left = Math.max(index[c], left); 
+            maxLength = Math.max(maxLength, right - left + 1);
+            index[c] = right + 1; 
+        }
+
+        return maxLength;
     }
 }
