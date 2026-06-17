ab6ca9066b4acc613ed8e6c5812d51532bba08ac
Brijesh Balaji S <143893586+BrijeshBalaji@users.noreply.github.com>
2026-06-17 16:30:19 +0530

    feat: Add Reverse Linked List

--- Linked List/Reverse Linked List/java/Solution.java
+++ Linked List/Reverse Linked List/java/Solution.java
@@ -1,15 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
+        ListNode prev= null;
         ListNode current = head;
-        
-        while (current != null) {
-            ListNode nextNode = current.next; // store next node
-            current.next = prev;              // reverse current node
-            prev = current;                   // move prev forward
-            current = nextNode;               // move current forward
+
+        while(current!=null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
         }
-        
-        return prev; // new head
+        return prev;
     }
 }
