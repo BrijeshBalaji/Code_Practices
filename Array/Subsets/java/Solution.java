689eed5e4f1509062912e77c0b65fe7a618680fe
Brijesh Balaji S <143893586+BrijeshBalaji@users.noreply.github.com>
2026-06-19 15:58:17 +0530

    feat: Add Subsets

--- Array/Subsets/java/Solution.java
+++ Array/Subsets/java/Solution.java
@@ -1,24 +1,17 @@
-import java.util.*;
-
 class Solution {
-
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums, 0);
-        return result;
+        List<List<Integer>> ans=new ArrayList<>();
+        sub(0,nums,ans,new ArrayList<>());
+        return ans;
     }
-
-    private void backtrack(List<List<Integer>> result, 
-                           List<Integer> tempList, 
-                           int[] nums, 
-                           int start) {
-
-        result.add(new ArrayList<>(tempList));
-
-        for (int i = start; i < nums.length; i++) {
-            tempList.add(nums[i]);          // choose
-            backtrack(result, tempList, nums, i + 1); // explore
-            tempList.remove(tempList.size() - 1);     // un-choose
+    public static void sub(int index,int nums[],List<List<Integer>> ans,List<Integer> temp){
+        if(index==nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
         }
+        temp.add(nums[index]);
+        sub(index+1,nums,ans,temp);
+        temp.remove(temp.size()-1);
+        sub(index+1,nums,ans,temp);
     }
 }
