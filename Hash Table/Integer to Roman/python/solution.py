91926e8c11a0c382a59947db9431d1004f175721
Brijesh Balaji S <143893586+BrijeshBalaji@users.noreply.github.com>
2026-06-26 16:38:34 +0530

    feat: Add Integer to Roman

---
+++ Hash Table/Integer to Roman/python/solution.py
@@ -0,0 +1,29 @@
+                sb.append(map.get(500));
+            }
+            else if(num-sum >=400 && num-sum <500)
+                sum+=500;
+                sb.append(map.get(1000));
+            }
+            else if(num-sum >= 500){
+                sum+=1000-100;
+                sb.append(map.get(100));
+            }
+            else if(num-sum>=900 && num-sum < 1000)
+            {
+              sum+=1000;
+              sb.append(map.get(1000));
+        while(sum<num){
+            if(num-sum >= 1000){
+        int sum=0;
+        
+        map.put(500,'D');
+        map.put(1000,'M');
+        map.put(100,'C');
+        map.put(50,'L');
+        map.put(10,'X');
+        map.put(5,'V');
+        map.put(1,'I');
+      StringBuilder sb = new StringBuilder();
+    public String intToRoman(int num) {
+      Map<Integer, Character> map = new HashMap<>();  
+class Solution {
