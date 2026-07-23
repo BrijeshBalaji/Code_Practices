ff33a549290d0aa23e69278036ec4fba74dc9ab4
Brijesh Balaji S <143893586+BrijeshBalaji@users.noreply.github.com>
2026-07-23 20:18:13 +0530

    feat: Add Valid Sudoku

--- Array/Valid Sudoku/java/Solution.java
+++ Array/Valid Sudoku/java/Solution.java
@@ -1,45 +1,28 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] row = new HashSet[9];
+        HashSet<Character>[] col = new HashSet[9];
+        HashSet<Character>[] boxes = new HashSet[9];
+
         for(int i=0;i<9;i++){
-            Set<Character> set = new HashSet<>();
+            row[i]=new HashSet<>();
+            col[i]=new HashSet<>();
+            boxes[i]=new HashSet<>();
+        }
+
+        for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                if(board[i][j]=='.'){
+                char ch = board[i][j];
+                if(ch == '.'){
                     continue;
                 }
-                if(set.contains(board[i][j])){
+                int box = (i/3)*3+(j/3);
+                if(row[i].contains(ch)||col[j].contains(ch)||boxes[box].contains(ch)){
                     return false;
                 }
-                set.add(board[i][j]);
-            }
-        }
-
-        for(int j=0;j<9;j++){
-            Set<Character> set = new HashSet<>();
-            for(int i=0;i<9;i++){
-                if(board[i][j]=='.'){
-                    continue;
-                }
-                if(set.contains(board[i][j])){
-                    return false;
-                }
-                set.add(board[i][j]);
-            }
-        }
-
-        for(int row=0;row<9;row+=3){
-            for(int col=0;col<9;col+=3){
-            Set<Character>set = new HashSet<>();
-                for(int i=row;i<row+3;i++){
-                    for(int j=col;j<col+3;j++){
-                        if(board[i][j]=='.'){
-                            continue;
-                        }
-                        if(set.contains(board[i][j])){
-                            return false;
-                        }
-                        set.add(board[i][j]);
-                    }
-                }
+                row[i].add(ch);
+                col[j].add(ch);
+                boxes[box].add(ch);
             }
         }
         return true;
