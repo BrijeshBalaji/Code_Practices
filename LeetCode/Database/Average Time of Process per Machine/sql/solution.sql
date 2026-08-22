ba47bb23f4a9764fad15539574c3e51a5c5cbd39
Brijesh Balaji S <143893586+BrijeshBalaji@users.noreply.github.com>
2026-08-22 14:40:12 +0530

    feat: Add Average Time of Process per Machine

--- LeetCode/Database/Average Time of Process per Machine/sql/solution.sql
+++ LeetCode/Database/Average Time of Process per Machine/sql/solution.sql
@@ -1,12 +1,17 @@
-# Write your MySQL query statement below
-select a1.machine_id , round(avg(a2.timestamp-a1.timestamp),3) as processing_time
-from activity a1
-join activity a2
-on a1.machine_id = a2.machine_id 
-and a1.activity_type = 'start'
-and a2.activity_type = 'end'
+# Write your MySQL query statement below
+select machine_id , 
+round(
+    avg(
+        case 
+          when activity_type = "end" then timestamp
+        end)-
+    avg(
+        case
+          when activity_type = "start" then timestamp
+        end
+    ),3)
+    as processing_time
+    from activity
+    group by machine_id;
 
-
-and a1.process_id=a2.process_id
-group by a1.machine_id;
 
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This repository contains my personal library of highly optimized, strictly teste
 ## 📊 Real-Time Performance Analytics
 
 <p align="center">
-  <img src="https://github-readme-leetcode-card.romitsagu.com/Brijesh_Balaji?theme=tokyonight&show=graph,recent&v=1787388682908" width="100%" />
+  <img src="https://github-readme-leetcode-card.romitsagu.com/Brijesh_Balaji?theme=tokyonight&show=graph,recent&v=1787389789067" width="100%" />
 </p>
 
 ## 📂 Repository Structure
